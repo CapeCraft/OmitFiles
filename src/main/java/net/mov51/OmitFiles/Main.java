5b382738458106e5eec9014c3e3ca8ae240ce4c9
mov51 <mov51@mov51.net>
2020-05-11 19:41:39 -0400

    Added comments and clarified variable names

--- src/main/java/net/mov51/OmitFiles/Main.java
+++ src/main/java/net/mov51/OmitFiles/Main.java
@@ -13,51 +13,51 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        if(args.length<2){
+        if(args.length<2){//If total passed args is smaller than 2 then exit
             System.out.println("Please fill out your arguments as follows");
             System.out.println("<Filter file path> <Direction in which to filter your credentials (hide|show)>");
-            System.exit(0);
+            System.exit(0);//Exits the jvm after error
         }
-        if (args[1].equals("hide")){
-            FilterAction(2,1, args[0]);
+        if (args[1].equals("hide")){//Checks the argument passed for desired output
+            //TODO Allow for different/better worded inputs
+            FilterAction(2,1, args[0]);//Passes replacement order to the FilterAction method
         }else if(args[1].equals("show")){
-            FilterAction(1,2, args[0]);
-        }else{
+            FilterAction(1,2, args[0]);//Passes replacement order to the FilterAction method
+        }else{//If no values match then print help and exit
             System.out.println("Please choose an option for argument 2!");
             System.out.println("<Direction in which to filter (hide|show)>");
-            System.exit(0);
+            System.exit(0);//Exits the jvm after error
         }
     }
 
     public static void FilterAction(int from, int to, String FileLocation){
-        Scanner scanner = null;
+        Scanner FilterFile = null;//Defines file scanner
         try {
-            scanner = new Scanner(new File(FileLocation));
+            FilterFile = new Scanner(new File(FileLocation));//Adds file location to scanner
         } catch (FileNotFoundException e) {
             System.out.println("Your file did not exist :(");
             System.out.println(FileLocation);
             System.out.println("Please pass the path to your file as the first argument!");
-            System.exit(0);
+            System.exit(0);//Exits the jvm after error
         }
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            // process the line
-            String[] SingleLine = line.split("-");
-            Path File = Paths.get(SingleLine[0]);
-            Charset charset = StandardCharsets.UTF_8;
-            String content = null;
+        while (FilterFile.hasNextLine()) {//Runs while FilterFile has another line to process
+            String line = FilterFile.nextLine();//Sets line to the current FilterFile line
+            String[] SingleLine = line.split("-");//Splits the filter line based on deliminator TODO Allow for different deliminators
+            Path File = Paths.get(SingleLine[0]);//Retrieves the path from the first section of the FilterFie line
+            Charset charset = StandardCharsets.UTF_8;//Defines the character set
+            String content = null;//Defines the content buffer string
             try {
-                content = new String(Files.readAllBytes(File), charset);
+                content = new String(Files.readAllBytes(File), charset);//places file path into a content buffer
             } catch (IOException e) {
                 e.printStackTrace();
-                System.exit(0);
+                System.exit(0);//Exits the jvm after error
             }
-            content = content.replace(SingleLine[from], SingleLine[to]);
+            content = content.replace(SingleLine[from], SingleLine[to]);//Replaces value 1 "from" with Value 2 "to"
             try {
-                Files.write(File, content.getBytes(charset));
+                Files.write(File, content.getBytes(charset));//Writes modified content buffer
             } catch (IOException e) {
                 e.printStackTrace();
-                System.exit(0);
+                System.exit(0);//Exits the jvm after error
             }
         }
     }
