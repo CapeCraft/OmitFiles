a1ee2914c7a57b9bc191fab8cc1d629bcf538bb2
James Harrison <james@fasttortoise.co.uk>
2020-07-05 10:23:45 +0100

    Cleaned up some Code Swiched to Lombok Log42J

--- src/main/java/net/mov51/OmitFiles/Filter.java
+++ src/main/java/net/mov51/OmitFiles/Filter.java
@@ -1,7 +1,6 @@
 package net.mov51.OmitFiles;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -10,42 +9,37 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Log4j2
 public class Filter {
 
-    static final Logger Log = Logger.getLogger(Filter.class);
+    public static void FilterAction(String filePath, String filter, String replacement) {
 
-    public static void FilterAction(String FilePath, String filter, String replacement){
-
-        if(Main.Debug == 1){
-            Logger.getRootLogger().setLevel(Level.DEBUG);
-        }
-
-        Log.debug("File path string is " + FilePath);
-        Path File = Paths.get(FilePath);//Retrieves the full path from the first section of the FilterFie line
-        Log.debug("File path path is " + File);
+        log.debug("File path string is {}", filePath);
+        Path file = Paths.get(filePath);//Retrieves the full path from the first section of the FilterFie line
+        log.debug("File path path is {}", file);
         Charset charset = StandardCharsets.UTF_8;//Defines the character set
         String content = null;//Defines the content buffer string
         try {
-            content = new String(Files.readAllBytes(File), charset);//places file path into a content buffer
+            content = new String(Files.readAllBytes(file), charset);//places file path into a content buffer
         } catch (IOException e) {
-            Log.error("Your target file to filter does not exist!");
-            Log.error(File.toString());//Prints attempted file path
+            log.error("Your target file to filter does not exist!");
+            log.error(file.toString());//Prints attempted file path
         }
-        Log.debug("Content filter is " + filter);
-        Log.debug("Content replacement is " + replacement);
+        log.debug("Content filter is {}", filter);
+        log.debug("Content replacement is {}", replacement);
         if (content != null) {
             content = content.replace(filter, replacement);//Replaces value 1 "from" with Value 2 "to"
-        }else{
-            Log.error("File To Filter \"" + FilePath + "\" is empty");
-            Log.error("It will not be filtered");
+        } else {
+            log.error("File To Filter {} is empty", filePath);
+            log.error("It will not be filtered");
         }
 
         try {
             assert content != null;
-            Files.write(File, content.getBytes(charset));//Writes modified content buffer
+            Files.write(file, content.getBytes(charset));//Writes modified content buffer
         } catch (IOException e) {
-            Log.error("The program failed to write the filtered file!");
-            Log.error(File);//Prints attempted file path
+            log.error("The program failed to write the filtered file!");
+            log.error(file);//Prints attempted file path
         }
 
     }
--- src/main/java/net/mov51/OmitFiles/ParseUtils/JSONtools.java
+++ src/main/java/net/mov51/OmitFiles/ParseUtils/JSONtools.java
@@ -1,46 +1,42 @@
 package net.mov51.OmitFiles.ParseUtils;
 
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import lombok.extern.log4j.Log4j2;
 import net.mov51.OmitFiles.Filter;
-import net.mov51.OmitFiles.Main;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+@Log4j2
 public class JSONtools {
 
-    static final Logger Log = Logger.getLogger(Filter.class);
-
-    public static void Parse(String from, String too, String FilterFile){
-
-        if(Main.Debug == 1){
-            Logger.getRootLogger().setLevel(Level.DEBUG);
-        }
+    public static void Parse(String from, String to, String filterFile) {
 
         Reader reader = null;//Defines reader
         try {
-            reader = new InputStreamReader(new FileInputStream(FilterFile));//Places file path into reader
+            reader = new InputStreamReader(new FileInputStream(filterFile));//Places file path into reader
         } catch (FileNotFoundException e) {
-            Log.error("Your file did not exist :(");
-            Log.error(FilterFile);//Prints attempted file path
-            Log.error("Please pass the path to your file as the first argument!");
+            log.error("Your file did not exist :(");
+            log.error(filterFile);//Prints attempted file path
+            log.error("Please pass the path to your file as the first argument!");
             e.printStackTrace();
             System.exit(0);//Exits the jvm after error
         }
-        Log.info("File found");
+        log.info("File found");
         JsonArray rootObj = JsonParser.parseReader(reader).getAsJsonArray();//Parses the reader into a JsonArray
-            for (JsonElement element : rootObj.getAsJsonArray()){//Loops through each element in the json array
-                Log.debug("Json object is " + element);
-                JsonObject filterObj = element.getAsJsonObject();//filters the element into a new json object
-                String FileToFilter = filterObj.get("file").getAsString();//Gets target file from json element
-                Log.debug(from + " > " + too);
-                String ReplaceFrom = filterObj.get(from).getAsString();//Uses passed from value to find either key or filter
-                String ReplaceToo = filterObj.get(too).getAsString();//Uses passed too value to find either key or filter
-                Filter.FilterAction(FileToFilter, ReplaceFrom, ReplaceToo);//calls filter action with parsed json element
-            }
+        for (JsonElement element : rootObj.getAsJsonArray()) {//Loops through each element in the json array
+            log.debug("Json object is " + element);
+            JsonObject filterObj = element.getAsJsonObject();//filters the element into a new json object
+            String fileToFilter = filterObj.get("file").getAsString();//Gets target file from json element
+            log.debug(String.format("%s > %s", from, to));
+            String replaceFrom = filterObj.get(from).getAsString();//Uses passed from value to find either key or filter
+            String replaceTo = filterObj.get(to).getAsString();//Uses passed too value to find either key or filter
+            Filter.FilterAction(fileToFilter, replaceFrom, replaceTo);//calls filter action with parsed json element
+        }
     }
 }
